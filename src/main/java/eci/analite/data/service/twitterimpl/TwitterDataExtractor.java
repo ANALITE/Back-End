ca8f513544322b46251346a7969e886b9c677c5e
Camilo Torres <camilo.torres-t@mail.escuelaing.edu.co>
2018-12-04 00:01:48 -0500

    adelanto text analytics

--- src/main/java/eci/analite/data/service/twitterimpl/TwitterDataExtractor.java
+++ src/main/java/eci/analite/data/service/twitterimpl/TwitterDataExtractor.java
@@ -5,9 +5,17 @@
  */
 package eci.analite.data.service.twitterimpl;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.net.ssl.HttpsURLConnection;
+import org.json.JSONObject;
 import twitter4j.Query;
 import twitter4j.QueryResult;
 import twitter4j.Status;
@@ -22,13 +30,12 @@ import twitter4j.TwitterFactory;
 public class TwitterDataExtractor {
 
     private static final Twitter TWITTER = TwitterFactory.getSingleton();
-    
+
     private static final String KEY = "d64c3ee8e9d940dd8a7d3573a79d279c";
     private static final String HOST = "https://eastus.api.cognitive.microsoft.com";
 
     private static final String PATH_SENTIMENT = "/text/analytics/v2.0/sentiment";
     private static final String PATH_KEYPHRASES = "/text/analytics/v2.0/keyPhrases";
-
 
     public static void search_data(String search_query) {
         try {
@@ -62,8 +69,82 @@ public class TwitterDataExtractor {
             String source = status.getSource().substring(low_bound, up_bound);
             String text = status.getText();
             Date date = status.getCreatedAt();
-            
+
             String line = String.format("%s,%s,%s,%s,%s", user, user_img, source, text, date.toString());
+        }
+    }
+
+    private static void get_sentiment(Documents documents, StringBuilder response) throws Exception {
+        JSONObject object = new JSONObject(documents);
+        String text = object.toString();
+        byte[] encoded_text = text.getBytes("UTF-8");
+
+        URL url = new URL(HOST + PATH_SENTIMENT);
+        HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-Type", "text/json");
+        connection.setRequestProperty("Ocp-Apim-Subscription-Key", KEY);
+        connection.setDoOutput(true);
+
+        DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
+        wr.write(encoded_text, 0, encoded_text.length);
+        wr.flush();
+        wr.close();
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(connection.getInputStream()));
+        String line;
+        while ((line = in.readLine()) != null) {
+            response.append(line);
+        }
+    }
+    
+    private static void get_keyphrases(Documents documents, StringBuilder response) throws Exception {
+        JSONObject object = new JSONObject(documents);
+        String text = object.toString();
+        byte[] encoded_text = text.getBytes("UTF-8");
+
+        URL url = new URL(HOST + PATH_KEYPHRASES);
+        HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-Type", "text/json");
+        connection.setRequestProperty("Ocp-Apim-Subscription-Key", KEY);
+        connection.setDoOutput(true);
+
+        DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
+        wr.write(encoded_text, 0, encoded_text.length);
+        wr.flush();
+        wr.close();
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(connection.getInputStream()));
+        String line;
+        while ((line = in.readLine()) != null) {
+            response.append(line);
+        }
+    }
+
+    private class Document {
+
+        public String id, language, text;
+
+        public Document(String id, String language, String text) {
+            this.id = id;
+            this.language = language;
+            this.text = text;
+        }
+    }
+
+    private class Documents {
+
+        public List<Document> documents;
+
+        public Documents() {
+            this.documents = new ArrayList<>();
+        }
+
+        public void add(String id, String language, String text) {
+            this.documents.add(new Document(id, language, text));
         }
     }
 }
